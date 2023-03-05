398b70d2d65b75b7510fb63c0676cc6f110c8a8a
unknown <boudrarfatimazahra>
2023-03-05 13:04:49 +0100

    add la somme

--- Problem1.js
+++ Problem1.js
@@ -3,6 +3,7 @@ var set2 = [2, 5, 4, 1, 9, 3]
 var result = []
 var i
 var j
+var sum = 0
 
 for (i = 0; i < set1.length; i++) {
     var Distinct = true
@@ -28,4 +29,9 @@ for (j = 0; j < set2.length; j++) {
         result.push(set2[j])
     }
 }
+//sum of distinct Arrays
 console.log(result);
+for (var y = 0; y < result.length; y++) {
+    sum += result[y]
+}
+console.log(sum);
